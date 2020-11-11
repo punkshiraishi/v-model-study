212bb7bec8f6a37d6917a36dd7e7719deeb7ba0b
punkshiraishi <pharmacy.sy@gmail.com>
2020-11-11 23:16:02 +0900

    Initial commit

---
+++ src/pages/DemoPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-10">
+    demo
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator"
+
+@Component({})
+export default class DemoPage extends Vue {}
+</script>
+
+<style scoped lang="scss" ></style>

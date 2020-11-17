81fb41786f0abe0c85497f7d5b919e76d86fd64d
shiraishi <shiraishi_yuma@uniquevision.co.jp>
2020-11-18 00:43:53 +0900

    add: broken base input

---
+++ src/components/BrokenBaseInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col space-y-2 w-1/4">
+    <input
+      v-model="value"
+      class="border-2 border-solid border-black px-1"
+    >
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator"
+
+@Component
+export default class BrokenBaseInput extends Vue {
+  @Prop({ required: true })
+  value!: string
+}
+</script>
